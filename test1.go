774fb04311532963c1103bf77d0d6fed7c8cd9ad
sashank-reddy <55399326+sashank-reddy@users.noreply.github.com>
2021-02-26 16:54:11 +0530

    update Test Cases

--- test1.go
+++ test1.go
@@ -1,12 +1,9 @@
-/*
+ /*
 Copyright 2014 Google Inc.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
      http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,35 +11,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// outyet is a web server that announces whether or not a particular Go version
+// has been tagged.
 package main
 
-import (
-     "fmt"
+import "fmt"
 
-     "golang.org/x/example/stringutil"
-)
+
+const URL = "Global"
 
 func main() {
-     var a,c = 10, 30
 
-     for i := 1; i < 5; i++ {
-          var z = "variable"
-          if i%2 != 0 { 
-          
-               const l,m = 1.2, "done"
-          }
+    var a = "initial"	
 
-     }
+    var b, c int = 1, 2
+    
+    
+	var i = 0 
+    for i < 3 { 
+       i += 2 
+       const e int = 10  
 
-     x := 100
- 
-     if x == 100 {
-          var p = 149
-          
-     } else {
-          const q ,r = "159", 10
-          println(q,r)
-      }
-          
-       
+    } 
 }
